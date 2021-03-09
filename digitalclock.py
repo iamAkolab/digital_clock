3ded1063d0754a2099cd89a6e30dacd64bfdf1d1
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-03-10 00:05:38 +0100

    added comments....

--- digitalclock.py
+++ digitalclock.py
@@ -1,20 +1,28 @@
 #from module import package
 
-from tkinter import  *
+from tkinter import *
 from time import strftime
 
+# creates a tinker window
 root = Tk()  
+
+# Adds title to tkinter window
 root.title("Digital Clock")
 
+# Function used display time on the label
 def time():
     string = strftime("%H:%M:%S  %p")
     lbl.config(text = string)
     lbl.after(1000, time)
 
-lbl = Label(root, font = "arial", 160, "bold", bg = "black", fg = white)
+#styling the label widget which displays the clock
+lbl = Label(root, font = "arial", 160, "bold", bg = "black", fg = "white")
 
+# pack method in tkinter pcks widgets into rows or columns, 
 lbl.pack(anchor="center", fill="both", expand = 1)
 
+# Time function is called
 time()
 
+# Runs the application program
 mainloop()
